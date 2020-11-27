16281d37b85aee8ce1ed213c84680de71d132e9a
fury_cydonian <ella.bir@hotmail.com>
2020-11-27 16:32:39 +0300

    add conftest.py and scripts.py

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+import requests
+
+
+@pytest.fixture
+def sw_people():
+    people = []
+    payload = {'page': 1}
+    response = requests.get('https://swapi.dev/api/people/', params=payload).json()
+    while response['next'] is not None:
+        response = requests.get('https://swapi.dev/api/people/', params=payload).json()
+        res_people = response['results']
+        people.extend(res_people)
+        payload['page'] += 1
+
+    return people
---
+++ scripts.py
@@ -0,0 +1,4 @@
+def is_unique(array):
+    if len(array) == len(set(array)):
+        return True
+    return False
