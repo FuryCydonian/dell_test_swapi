9c5b815af13ce98fedda5d9956a72f250c4fdf54
fury_cydonian <ella.bir@hotmail.com>
2020-11-27 16:26:44 +0300

    reorganize project and add test_unique_names

--- test_swapi.py
+++ test_swapi.py
@@ -1,14 +1,14 @@
 import requests
-import pytest
-
-
-@pytest.fixture
-def sw_people():
-    people = requests.get('https://swapi.dev/api/people/').json()['results']
-    print(people)
-    return people
+import scripts
 
 
 def test_length_array_people(sw_people):
     response_people = requests.get('https://swapi.dev/api/people/').json()
     assert len(sw_people) == response_people['count']
+
+
+def test_unique_names(sw_people):
+    names = []
+    for creature in sw_people:
+        names.append(creature['name'])
+    assert scripts.is_unique(names)
