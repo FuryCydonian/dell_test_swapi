ae807c4224ed09a035e310c78f671b7851714979
fury_cydonian <ella.bir@hotmail.com>
2020-11-28 23:37:36 +0300

    add factory fixture for search people

--- conftest.py
+++ conftest.py
@@ -14,6 +14,15 @@ def sw_people():
         payload['page'] += 1
     return people
 
+
 @pytest.fixture
 def sw_people_schema():
     return requests.get('https://swapi.dev/api/people/schema').json()
+
+
+@pytest.fixture
+def search_result():
+    def _search_result(request):
+        payload = {'search': request}
+        return requests.get('https://swapi.dev/api/people/', params=payload).json()
+    return _search_result
